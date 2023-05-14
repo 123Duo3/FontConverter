764ea08ae34d7f5a119070e67a4b93a091eb02cf
123Duo3 <468673794@qq.com>
2023-05-14 23:40:27 +0800

    Support sans serif styles

--- app/src/main/kotlin/ink/duo3/fontconverter/utils/StyleText.kt
+++ app/src/main/kotlin/ink/duo3/fontconverter/utils/StyleText.kt
@@ -12,6 +12,10 @@ fun String.toStyled(style: TextStyle): String {
                 TextStyle.BOLD -> c.toBold()
                 TextStyle.ITALIC -> c.toItalic()
                 TextStyle.BOLD_ITALIC -> c.toBoldItalic()
+                TextStyle.SANS_NORMAL -> c.toSansNormal()
+                TextStyle.SANS_BOLD -> c.toSansBold()
+                TextStyle.SANS_ITALIC -> c.toSansItalic()
+                TextStyle.SANS_BOLD_ITALIC -> c.toSansBoldItalic()
                 else -> c
             }
         )
@@ -23,6 +27,10 @@ enum class TextStyle {
     BOLD,
     ITALIC,
     BOLD_ITALIC,
+    SANS_NORMAL,
+    SANS_BOLD,
+    SANS_ITALIC,
+    SANS_BOLD_ITALIC
 }
 
 @Composable
@@ -31,6 +39,10 @@ fun TextStyle.toName(): String {
         TextStyle.BOLD -> stringResource(id = R.string.font_bold)
         TextStyle.ITALIC -> stringResource(id = R.string.font_italic)
         TextStyle.BOLD_ITALIC -> stringResource(id = R.string.font_bold_italic)
+        TextStyle.SANS_NORMAL -> stringResource(id = R.string.font_sans_normal)
+        TextStyle.SANS_BOLD -> stringResource(id = R.string.font_sans_bold)
+        TextStyle.SANS_ITALIC -> stringResource(id = R.string.font_sans_italic)
+        TextStyle.SANS_BOLD_ITALIC -> stringResource(id = R.string.font_sans_bold_italic)
     }
 }
 
@@ -40,58 +52,11 @@ fun Char.toBold(): String {
         in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDC1A).toChar()}"
         in '0'..'9' -> "\uD835${(this.code - '0'.code + 0xDFCE).toChar()}"
         // Capitals of Greek Alphabet
-        'Α' -> "\uD835\uDEA8"
-        'Β' -> "\uD835\uDEA9"
-        'Γ' -> "\uD835\uDEAA"
-        'Δ' -> "\uD835\uDEAB"
-        'Ε' -> "\uD835\uDEAC"
-        'Ζ' -> "\uD835\uDEAD"
-        'Η' -> "\uD835\uDEAE"
-        'Θ' -> "\uD835\uDEAF"
-        'Ι' -> "\uD835\uDEB0"
-        'Κ' -> "\uD835\uDEB1"
-        'Λ' -> "\uD835\uDEB2"
-        'Μ' -> "\uD835\uDEB3"
-        'Ν' -> "\uD835\uDEB4"
-        'Ξ' -> "\uD835\uDEB5"
-        'Ο' -> "\uD835\uDEB6"
-        'Π' -> "\uD835\uDEB7"
-        'Ρ' -> "\uD835\uDEB8"
         'ϴ' -> "\uD835\uDEB9"
-        'Σ' -> "\uD835\uDEBA"
-        'Τ' -> "\uD835\uDEBB"
-        'Υ' -> "\uD835\uDEBC"
-        'Φ' -> "\uD835\uDEBD"
-        'Χ' -> "\uD835\uDEBE"
-        'Ψ' -> "\uD835\uDEBF"
-        'Ω' -> "\uD835\uDEC0"
+        in 'Α'..'Ω' -> "\uD835${(this.code - 'Α'.code + 0xDEA8).toChar()}"
         '∇' -> "\uD835\uDEC1"
         // Lowercase of Greek Alphabet
-        'α' -> "\uD835\uDEC2"
-        'β' -> "\uD835\uDEC3"
-        'γ' -> "\uD835\uDEC4"
-        'δ' -> "\uD835\uDEC5"
-        'ε' -> "\uD835\uDEC6"
-        'ζ' -> "\uD835\uDEC7"
-        'η' -> "\uD835\uDEC8"
-        'θ' -> "\uD835\uDEC9"
-        'ι' -> "\uD835\uDECA"
-        'κ' -> "\uD835\uDECB"
-        'λ' -> "\uD835\uDECC"
-        'μ' -> "\uD835\uDECD"
-        'ν' -> "\uD835\uDECE"
-        'ξ' -> "\uD835\uDECF"
-        'ο' -> "\uD835\uDED0"
-        'π' -> "\uD835\uDED1"
-        'ρ' -> "\uD835\uDED2"
-        'ς' -> "\uD835\uDED3"
-        'σ' -> "\uD835\uDED4"
-        'τ' -> "\uD835\uDED5"
-        'υ' -> "\uD835\uDED6"
-        'φ' -> "\uD835\uDED7"
-        'χ' -> "\uD835\uDED8"
-        'ψ' -> "\uD835\uDED9"
-        'ω' -> "\uD835\uDEDA"
+        in 'α'..'ω' -> "\uD835${(this.code - 'α'.code + 0xDEC2).toChar()}"
         '∂' -> "\uD835\uDEDB"
         'ϵ' -> "\uD835\uDEDC"
         'ϑ' -> "\uD835\uDEDD"
@@ -112,6 +77,19 @@ fun Char.toItalic(): String {
         'h' -> "\u210E"
         in 'A'..'Z' -> "\uD835${(this.code - 'A'.code + 0xDC34).toChar()}"
         in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDC4E).toChar()}"
+        // Capitals of Greek Alphabet
+        'ϴ' -> "\uD835\uDEF3"
+        in 'Α'..'Ω' -> "\uD835${(this.code - 'Α'.code + 0xDEE2).toChar()}"
+        '∇' -> "\uD835\uDEFB"
+        // Lowercase of Greek Alphabet
+        in 'α'..'ω' -> "\uD835${(this.code - 'α'.code + 0xDEFC).toChar()}"
+        '∂' -> "\uD835\uDF15"
+        'ϵ' -> "\uD835\uDF16"
+        'ϑ' -> "\uD835\uDF17"
+        'ϰ' -> "\uD835\uDF18"
+        'ϕ' -> "\uD835\uDF19"
+        'ϱ' -> "\uD835\uDF1A"
+        'ϖ' -> "\uD835\uDF1B"
         else -> this.toString()
     }
 }
@@ -120,6 +98,78 @@ fun Char.toBoldItalic(): String {
     return when (this) {
         in 'A'..'Z' -> "\uD835${(this.code - 'A'.code + 0xDC68).toChar()}"
         in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDC82).toChar()}"
+        // Capitals of Greek Alphabet
+        'ϴ' -> "\uD835\uDF2D"
+        in 'Α'..'Ω' -> "\uD835${(this.code - 'Α'.code + 0xDF1C).toChar()}"
+        '∇' -> "\uD835\uDF35"
+        // Lowercase of Greek Alphabet
+        in 'α'..'ω' -> "\uD835${(this.code - 'α'.code + 0xDF36).toChar()}"
+        '∂' -> "\uD835\uDF4F"
+        'ϵ' -> "\uD835\uDF50"
+        'ϑ' -> "\uD835\uDF51"
+        'ϰ' -> "\uD835\uDF52"
+        'ϕ' -> "\uD835\uDF53"
+        'ϱ' -> "\uD835\uDF54"
+        'ϖ' -> "\uD835\uDF55"
+        else -> this.toString()
+    }
+}
+
+fun Char.toSansNormal(): String {
+    return when (this) {
+        in 'A'..'Z' -> "\uD835${(this.code - 'A'.code + 0xDDA0).toChar()}"
+        in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDDBA).toChar()}"
+        in '0'..'9' -> "\uD835${(this.code - '0'.code + 0xDFE2).toChar()}"
+        else -> this.toString()
+    }
+}
+fun Char.toSansBold(): String {
+    return when (this) {
+        in 'A'..'Z' -> "\uD835${(this.code - 'A'.code + 0xDDD4).toChar()}"
+        in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDDEE).toChar()}"
+        in '0'..'9' -> "\uD835${(this.code - '0'.code + 0xDFEC).toChar()}"
+        // Capitals of Greek Alphabet
+        'ϴ' -> "\uD835\uDF67"
+        in 'Α'..'Ω' -> "\uD835${(this.code - 'Α'.code + 0xDF56).toChar()}"
+        '∇' -> "\uD835\uDF6F"
+        // Lowercase of Greek Alphabet
+        in 'α'..'ω' -> "\uD835${(this.code - 'α'.code + 0xDF70).toChar()}"
+        '∂' -> "\uD835\uDF89"
+        'ϵ' -> "\uD835\uDF8A"
+        'ϑ' -> "\uD835\uDF8B"
+        'ϰ' -> "\uD835\uDF8C"
+        'ϕ' -> "\uD835\uDF8D"
+        'ϱ' -> "\uD835\uDF8E"
+        'ϖ' -> "\uD835\uDF8F"
+
+        else -> this.toString()
+    }
+}
+fun Char.toSansItalic(): String {
+    return when (this) {
+        in 'A'..'Z' -> "\uD835${(this.code - 'A'.code + 0xDE08).toChar()}"
+        in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDE22).toChar()}"
+        else -> this.toString()
+    }
+}
+fun Char.toSansBoldItalic(): String {
+    return when (this) {
+        in 'A'..'Z' -> "\uD835${(this.code - 'A'.code + 0xDE3C).toChar()}"
+        in 'a'..'z' -> "\uD835${(this.code - 'a'.code + 0xDE56).toChar()}"
+        // Capitals of Greek Alphabet
+        'ϴ' -> "\uD835\uDFA1"
+        in 'Α'..'Ω' -> "\uD835${(this.code - 'Α'.code + 0xDF90).toChar()}"
+        '∇' -> "\uD835\uDFA9"
+        // Lowercase of Greek Alphabet
+        in 'α'..'ω' -> "\uD835${(this.code - 'α'.code + 0xDFAA).toChar()}"
+        '∂' -> "\uD835\uDFC3"
+        'ϵ' -> "\uD835\uDFC4"
+        'ϑ' -> "\uD835\uDFC5"
+        'ϰ' -> "\uD835\uDFC6"
+        'ϕ' -> "\uD835\uDFC7"
+        'ϱ' -> "\uD835\uDFC8"
+        'ϖ' -> "\uD835\uDFC9"
+
         else -> this.toString()
     }
 }
